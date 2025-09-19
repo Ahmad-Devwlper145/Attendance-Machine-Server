5eacc2ec9c615234c1e5b7ede0087b76de6d43f0
Ahmad-Devwlper145 <ahmad.develper125@gmail.com>
2025-09-19 11:37:38 +0500

    11:37 3rd

--- server.js
+++ server.js
@@ -5,14 +5,41 @@ const PORT = process.env.PORT || 3000;
 // Middleware to parse JSON bodies
 app.use(express.json());
 
+
+// Usage Of Server
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    console.log('Headers:', JSON.stringify(req.headers, null, 2));
+    console.log('Body:', JSON.stringify(req.body, null, 2));
+    next();
+});
+
+
+
+
 // Basic route
 app.get('/', (req, res) => {
     res.json({
         message: 'Welcome to Tiny Biometric Attendance Machine Server',
-        status: 'Server is running successfully!',
+        status: 200,
+        serStatus: 'Server is running successfully!',
         timestamp: new Date().toISOString()
     });
 });
+
+
+
+
+// Basic route
+app.get('/tst', (req, res) => {
+    res.json({
+        status:200,
+        message: 'TST Path',
+    });
+});
+
+
+
 
 // Start the server
 app.listen(PORT, () => {
